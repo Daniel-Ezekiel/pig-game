4926eae2dc54bc94d94a54cf21dc7a3afecb8c00
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-01-18 18:01:04 +0100

    chore: refactored codebase and made it more readable

--- script.js
+++ script.js
@@ -10,6 +10,22 @@ const btnReset = document.querySelector('.btn-reset');
 const player1 = document.querySelector('.player1');
 const player2 = document.querySelector('.player2');
 
+function switchPlayer() {
+  const player1Status = player1.classList.contains('active');
+
+  if (player1Status) {
+    player1.classList.remove('active');
+    player2.classList.add('active');
+  } else {
+    player2.classList.remove('active');
+    player1.classList.add('active');
+  }
+}
+
+function changeTextContent(className, content) {
+  document.querySelector(className).textContent = content;
+}
+
 btnRoll.addEventListener('click', rollDice);
 function rollDice() {
   const rollValue = Math.ceil(Math.random() * 6);
@@ -19,70 +35,60 @@ function rollDice() {
   dieImg.src = `./img/dice-${rollValue}.png`;
   document.querySelector('main').appendChild(dieImg);
 
-  if (player1.classList.contains('active') && rollValue > 1) {
-    currentScore[0] += rollValue;
-    document.querySelector('.player1-current').textContent = currentScore[0];
-  } else if (player2.classList.contains('active') && rollValue > 1) {
-    currentScore[1] += rollValue;
-    document.querySelector('.player2-current').textContent = currentScore[1];
-  } else if (player1.classList.contains('active') && !(rollValue > 1)) {
-    currentScore[0] = 0;
-    document.querySelector('.player1-current').textContent = currentScore[0];
+  const isPlayer1Active = player1.classList.contains('active');
 
-    player1.classList.remove('active');
-    player2.classList.add('active');
+  if (rollValue > 1) {
+    isPlayer1Active
+      ? (currentScore[0] += rollValue)
+      : (currentScore[1] += rollValue);
+    changeTextContent('.player1-current', currentScore[0]);
+    changeTextContent('.player2-current', currentScore[1]);
   } else {
-    currentScore[1] = 0;
-    document.querySelector('.player2-current').textContent = currentScore[1];
+    isPlayer1Active ? (currentScore[0] = 0) : (currentScore[1] = 0);
+    changeTextContent('.player1-current', currentScore[0]);
+    changeTextContent('.player2-current', currentScore[1]);
 
-    player2.classList.remove('active');
-    player1.classList.add('active');
+    switchPlayer();
   }
 }
 
 btnHold.addEventListener('click', holdScore);
 function holdScore() {
-  if (player1.classList.contains('active')) {
+  const isPlayer1Active = player1.classList.contains('active');
+
+  if (isPlayer1Active) {
     totalScore[0] += currentScore[0];
-    document.querySelector('.player1-score').textContent = totalScore[0];
-
-    player1.classList.remove('active');
-    player2.classList.add('active');
-
+    changeTextContent('.player1-score', totalScore[0]);
     currentScore[0] = 0;
-    document.querySelector('.player1-current').textContent = currentScore[0];
+    changeTextContent('.player1-current', currentScore[0]);
 
+    switchPlayer();
     if (totalScore[0] >= 100)
-      document.querySelector('.player1-current').textContent = 'WINNER! 🏆';
+      changeTextContent('.player1-current', 'WINNER! 🏆');
   } else {
     totalScore[1] += currentScore[1];
-    document.querySelector('.player2-score').textContent = totalScore[1];
-
-    player2.classList.remove('active');
-    player1.classList.add('active');
-
+    changeTextContent('.player2-score', totalScore[1]);
     currentScore[1] = 0;
-    document.querySelector('.player2-current').textContent = currentScore[1];
+    changeTextContent('.player2-current', currentScore[1]);
 
+    switchPlayer();
     if (totalScore[1] >= 100)
-      document.querySelector('.player2-current').textContent = 'WINNER! 🏆';
+      changeTextContent('.player2-current', 'WINNER! 🏆');
   }
 }
 
 btnReset.addEventListener('click', startNewGame);
 function startNewGame() {
   currentScore = [0, 0];
+  changeTextContent('.player1-current', currentScore[0]);
+  changeTextContent('.player2-current', currentScore[1]);
+
   totalScore = [0, 0];
-
-  document.querySelector('.player1-current').textContent = currentScore[0];
-  document.querySelector('.player2-current').textContent = currentScore[1];
-
-  document.querySelector('.player1-score').textContent = totalScore[0];
-  document.querySelector('.player2-score').textContent = totalScore[1];
+  changeTextContent('.player1-score', totalScore[0]);
+  changeTextContent('.player2-score', totalScore[1]);
 
   const dieImg = document.querySelectorAll('.die-img');
   if (dieImg) dieImg.forEach(dieImg => dieImg.remove());
 
-  player1.classList.add('active');
-  player2.classList.remove('active');
+  switchPlayer();
 }
